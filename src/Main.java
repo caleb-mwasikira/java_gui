5a2fe34599a8764f9fc2b48bf47e5a725cdc9958
Caleb Mwasikira <calebmwasikira@gmail.com>
2024-06-07 08:08:34 +0300

    Closing frame with Process Window Event

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,24 @@
 import javax.swing.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("My First Desktop App");
+        JFrame frame = new MyJFrame("Closing frame with Process Window Event");
         frame.setBounds(50, 50, 600, 400);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+    }
+}
+
+class MyJFrame extends JFrame {
+    MyJFrame(String title) {
+        this.setTitle(title);
+    }
+
+    protected void processWindowEvent(WindowEvent e) {
+        if(e.getID() == WindowEvent.WINDOW_CLOSING) {
+            System.exit(0);
+        }
     }
 }
