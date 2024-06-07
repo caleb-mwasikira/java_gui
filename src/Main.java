28a9f2b5320b6715fd7783d95a87756e58a1e9f3
Caleb Mwasikira <calebmwasikira@gmail.com>
2024-06-07 07:46:31 +0300

    Java Swing Hello World

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("My First Desktop App");
+        frame.setBounds(50, 50, 600, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
