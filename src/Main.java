4dd3a8a338eaeec98e91f1519c16273d3af34788
Caleb Mwasikira <calebmwasikira@gmail.com>
2024-06-19 15:19:01 +0300

    simple counter program

--- src/Main.java
+++ src/Main.java
@@ -4,82 +4,66 @@ import java.awt.event.*;
 
 public class Main {
     public static void main(String[] args) {
-        new MyJFrame("Handling mouse events");
+        new MyJFrame("counter");
     }
 }
 
-class MyWindowListener extends WindowAdapter {
-    public void windowClosing(WindowEvent e) {
-        System.out.println("Closing application. Bye :(");
-        System.exit(0);
-    }
-}
+class MyJFrame extends JFrame {
+    int count = 0;
+    int frameWidth = 700;
+    int frameHeight = 400;
 
-class MyJFrame extends JFrame implements MouseListener, MouseMotionListener {
-    String msg = "";
-    Point mousePos = new Point();
+    JLabel label;
 
     public MyJFrame(String title) {
-        setSize(500, 400);
+        setSize(frameWidth, frameHeight);
         setTitle(title);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null); // exlicitly setting bounds for all our components
 
-        addWindowListener(new MyWindowListener());
-        addMouseListener(this);
-        addMouseMotionListener(this);
+        Container container = getContentPane();
+
+        label = new JLabel(getMessage());
+        label.setBounds((int)(0.25 * frameWidth), 25, frameWidth, 50);
+        container.add(label);
+
+        JButton addButton = new JButton("Add");
+        addButton.setBounds((int)(0.25 * frameWidth), 75, 100, 25);
+        addButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                count++;
+                label.setText(getMessage());
+            }
+        });
+        container.add(addButton);
+
+        JButton resetBtn = new JButton("Reset");
+        resetBtn.setBounds((int)(0.25 * frameWidth) + 120, 75, 100, 25);
+        resetBtn.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                count = 0;
+                label.setText(getMessage());
+            }
+        });
+        container.add(resetBtn);
+
+        JButton subtractButton = new JButton("Subtract");
+        subtractButton.setBounds((int)(0.25 * frameWidth) + 240, 75, 100, 25);
+        subtractButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                count--;
+                label.setText(getMessage());
+            }
+        });
+        container.add(subtractButton);
 
         setVisible(true);
     }
 
-    @Override
-    public void mouseClicked(MouseEvent mouseEvent) {
-        msg = msg + "-- mouse clicked";
-        repaint();
-    }
-
-    @Override
-    public void mousePressed(MouseEvent mouseEvent) {
-        // save the coordinates
-        mousePos = mouseEvent.getPoint();
-        msg = "mouse pressed";
-        repaint();
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent mouseEvent) {
-        // save the coordinates
-        mousePos = mouseEvent.getPoint();
-        msg = "mouse released";
-        repaint();
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent mouseEvent) {
-        msg = "mouse entered";
-        repaint();
-    }
-
-    @Override
-    public void mouseExited(MouseEvent mouseEvent) {
-        msg = "mouse exited";
-        repaint();
-    }
-
-    @Override
-    public void mouseDragged(MouseEvent mouseEvent) {
-        mousePos = mouseEvent.getPoint();
-        msg = "* mouse dragged X: " + mousePos.x + " Y: " + mousePos.y;
-        repaint();
-    }
-
-    @Override
-    public void mouseMoved(MouseEvent mouseEvent) {
-        mousePos = mouseEvent.getPoint();
-        msg = "* moving mouse X: " + mousePos.x + " Y: " + mousePos.y;
-        repaint();
-    }
-
-    @Override
-    public void paint(Graphics g) {
-        g.drawString(msg, mousePos.x, mousePos.y);
+    String getMessage() {
+        return String.format("Count is now at %d", count);
     }
 }
