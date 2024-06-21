71626e789ce442c30657f74ee97dc61c48924b2f
Caleb Mwasikira <calebmwasikira@gmail.com>
2024-06-21 09:51:21 +0300

    radio buttons and checkbox example

--- src/Main.java
+++ src/Main.java
@@ -1,69 +1,51 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
-import java.awt.event.*;
 
 public class Main {
     public static void main(String[] args) {
-        new MyJFrame("counter");
+        new MyJFrame("radio button and checkbox example");
     }
 }
 
 class MyJFrame extends JFrame {
-    int count = 0;
-    int frameWidth = 700;
+    int frameWidth = 500;
     int frameHeight = 400;
 
-    JLabel label;
-
     public MyJFrame(String title) {
+        super(title);
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new GridLayout(0, 2));
+
+        // radio button panel
+        JPanel panel = new JPanel(new GridLayout(0, 1));
+        Border border = BorderFactory.createTitledBorder("Crystal Shopping");
+        panel.setBorder(border);
+
+        ButtonGroup group = new ButtonGroup();
+
+        String[] items = {"Salt", "Sugar", "Flour"};
+        for (String item: items) {
+            JRadioButton btn = new JRadioButton(item);
+            group.add(btn);
+            panel.add(btn);
+        }
+        contentPane.add(panel);
+
+        // checkbox button panel
+        panel = new JPanel(new GridLayout(0, 1));
+        border = BorderFactory.createTitledBorder("Vegetable shopping");
+        panel.setBorder(border);
+
+        items = new String[]{"SukumaWiki", "Cabbage", "Spinach", "Kienyeji"};
+        for(String item: items) {
+            JCheckBox checkbox = new JCheckBox(item);
+            panel.add(checkbox);
+        }
+        contentPane.add(panel);
+
         setSize(frameWidth, frameHeight);
-        setTitle(title);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(null); // exlicitly setting bounds for all our components
-
-        Container container = getContentPane();
-
-        label = new JLabel(getMessage());
-        label.setBounds((int)(0.25 * frameWidth), 25, frameWidth, 50);
-        container.add(label);
-
-        JButton addButton = new JButton("Add");
-        addButton.setBounds((int)(0.25 * frameWidth), 75, 100, 25);
-        addButton.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                count++;
-                label.setText(getMessage());
-            }
-        });
-        container.add(addButton);
-
-        JButton resetBtn = new JButton("Reset");
-        resetBtn.setBounds((int)(0.25 * frameWidth) + 120, 75, 100, 25);
-        resetBtn.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                count = 0;
-                label.setText(getMessage());
-            }
-        });
-        container.add(resetBtn);
-
-        JButton subtractButton = new JButton("Subtract");
-        subtractButton.setBounds((int)(0.25 * frameWidth) + 240, 75, 100, 25);
-        subtractButton.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                count--;
-                label.setText(getMessage());
-            }
-        });
-        container.add(subtractButton);
-
         setVisible(true);
-    }
-
-    String getMessage() {
-        return String.format("Count is now at %d", count);
     }
 }
