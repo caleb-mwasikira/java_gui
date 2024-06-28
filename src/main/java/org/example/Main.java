7e411d8401edd37454edc28349745405a06eae58
Caleb Mwasikira <calebmwasikira@gmail.com>
2024-06-28 08:48:20 +0300

    scrollpane example

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,96 +4,36 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
-        new MyJFrame("login form");
+        new MyJFrame("ScrollPane example");
     }
 }
 
 class MyJFrame extends JFrame {
     int frameWidth = 500;
     int frameHeight = 400;
-    Container contentPane;
-    JTextField emailField;
-    JPasswordField passwordField;
 
-    public MyJFrame(String title) {
+    MyJFrame(String title) {
         super(title);
+        setSize(frameWidth, frameHeight);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
 
-        contentPane = getContentPane();
-        contentPane.setLayout(null);
-        setComponents();
+        JPanel topPanel = new JPanel();
 
-        setMinimumSize(new Dimension(frameWidth, frameHeight));
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Path iconPath = Paths.get("/home/netrunner/Pictures/profile_pics/wallguy.png");
+        Icon image = new ImageIcon(iconPath.toString());
+        JLabel label = new JLabel(image);
+
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.getViewport().add(label);
+        topPanel.add(scrollPane, BorderLayout.CENTER);
+
+        getContentPane().add(topPanel);
         setVisible(true);
-    }
-
-    public void setComponents() {
-        // email
-        JPanel panel = new JPanel(new FlowLayout());
-
-        JLabel emailLabel = new JLabel("Email: ");
-        panel.add(emailLabel);
-        emailField = new JTextField(null, 20);
-        panel.add(emailField);
-
-        panel.setBounds(20, 20, 400, 50);
-        contentPane.add(panel);
-
-        // password
-        panel = new JPanel(new FlowLayout());
-
-        JLabel passwordLabel = new JLabel("Password: ");
-        panel.add(passwordLabel);
-        passwordField = new JPasswordField(null, 20);
-        panel.add(passwordField);
-
-        panel.setBounds(5, 70, 400, 50);
-        contentPane.add(panel);
-
-        // buttons
-        panel = new JPanel(new FlowLayout());
-
-        JButton clearBtn = new JButton("Clear");
-        clearBtn.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                emailField.setText("");
-                passwordField.setText("");
-            }
-        });
-        panel.add(clearBtn);
-        JButton loginBtn = new JButton("Login");
-        loginBtn.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                handleLogin();
-            }
-        });
-        panel.add(loginBtn);
-
-        panel.setBounds(15, 120, 400, 50);
-        contentPane.add(panel);
-    }
-
-    public String join(char[] chars) {
-        StringBuilder str = new StringBuilder();
-        for (char ch: chars) {
-            str.append(ch);
-        }
-        return str.toString();
-    }
-
-    public void handleLogin() {
-        String email = emailField.getText();
-        String password = join(passwordField.getPassword());
-
-        if(email.equals("johndoe@gmail.com") && password.equals("password")) {
-            JOptionPane.showMessageDialog(contentPane, "Login success");
-        } else {
-            JOptionPane.showMessageDialog(contentPane, "Login failed");
-        }
     }
 }
